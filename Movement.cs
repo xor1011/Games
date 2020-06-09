de606c9ca59ed240bab0a95464709c4684ef3c69
Martin Johnson <42688266+xor1011@users.noreply.github.com>
2020-06-08 19:08:03 -1000

    Add files via upload
    This was a quick 2D platformer game I made the week before Thanksgiving, basically it's a character with my Boss's face running around chopping turkeys with an ax. You can play it at Itch.io/kodierer the name is Turkey Hunter. Feel free to improve it, add extra levels and anything else you like. It's free to the world.

---
+++ Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float maxSpeed = 25f;
+    public bool facingLeft = false;
+    public Transform character;
+    public Animator anim;
+    private float jump = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed", Mathf.Abs(move));
+     
+        GetComponent<Rigidbody2D>().velocity = new Vector3(move * maxSpeed * Time.deltaTime, GetComponent<Rigidbody2D>().velocity.y);
+        if (move < 0f && !facingLeft)
+        {
+
+            Flip();
+        }
+        else if (move > 0f && facingLeft)
+        {
+            Flip();
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetBool("chop", true);
+        }
+        else if(Input.GetButtonUp("Fire1"))
+        {
+            anim.SetBool("chop", false);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+            if (jump == 4f) jump = -2f;
+            else jump = 4f;
+        }
+      
+    }
+    
+    
+
+
+    //flip if needed
+    void Flip()
+    {
+        facingLeft = !facingLeft;
+        character.Rotate(0, 180f, 0);
+        
+       
+        
+    }
+
+
+    
+}
---
+++ enemyAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAI : MonoBehaviour
+{
+    public float enemySpeed = 40;
+    private Transform target;
+    public bool FacingLeft = true;
+    Movement movement = new Movement();
+    public Transform enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (target.position.x > transform.position.x && FacingLeft == true)
+        {
+            FacingLeft = false;
+            transform.Rotate(0, 180f, 0);
+        }
+        else if (target.position.x < transform.position.x && FacingLeft == false)
+        {
+            FacingLeft = true;
+            transform.Rotate(0, 180f, 0);
+        }
+
+    }
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, enemySpeed * Time.deltaTime);
+    }
+}
